e706bb18a997ec464bed2bade92e5acc3810e126
MDud <duda915@gmail.com>
2019-02-26 14:33:34 +0100

    spring database setup, schema.sql for db initialization

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS forum_user (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password CHAR(60) NOT NULL,
+  image VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS user_authority (
+  id SERIAL PRIMARY KEY,
+  forum_user_id INTEGER REFERENCES forum_user(id),
+  authority VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS topic (
+  id SERIAL PRIMARY KEY,
+  original_poster INTEGER REFERENCES forum_user(id),
+  title TEXT
+);
+
+CREATE TABLE IF NOT EXISTS post (
+  id SERIAL PRIMARY KEY,
+  poster INTEGER REFERENCES forum_user(id),
+  topic_id INTEGER REFERENCES topic(id),
+  content TEXT
+);
