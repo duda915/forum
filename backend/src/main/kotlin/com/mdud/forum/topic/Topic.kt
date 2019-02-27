a0f88974cb057ef0b70b35de22b8010c569fea50
MDud <duda915@gmail.com>
2019-02-27 15:15:47 +0100

    Moved topic to separate package

---
+++ backend/src/main/kotlin/com/mdud/forum/topic/Topic.kt
@@ -0,0 +1,24 @@
+package com.mdud.forum.topic
+
+import com.mdud.forum.topic.post.Post
+import com.mdud.forum.user.User
+import javax.persistence.*
+
+@Entity
+@Table(name = "topic")
+data class Topic(
+        @ManyToOne(cascade = [CascadeType.MERGE])
+        @JoinColumn(referencedColumnName = "original_poster")
+        val originalPoster: User = User(),
+
+        @Column(name = "title")
+        val title: String = "",
+
+        @OneToMany(cascade = [CascadeType.ALL])
+        @JoinColumn(name = "topic_id")
+        val posts: MutableList<Post> = mutableListOf()
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+}
