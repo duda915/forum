7fbb2be7ace83bfdf74b04a62d5f7219edb31169
MDud <duda915@gmail.com>
2019-03-05 11:11:48 +0100

    UserController integration tests

--- backend/src/test/kotlin/com/mdud/forum/integration/user/UserControllerIntegrationTest.kt
+++ backend/src/test/kotlin/com/mdud/forum/integration/user/UserControllerIntegrationTest.kt
@@ -1,25 +1,32 @@
 package com.mdud.forum.integration.user
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.JsonPath
 import com.mdud.forum.integration.IntegrationProfileValueSource
 import com.mdud.forum.integration.TokenTestHelper
+import com.mdud.forum.staticresource.StaticResourceService
+import com.mdud.forum.user.PasswordDTO
 import com.mdud.forum.user.UserDTO
+import com.mdud.forum.user.UserService
 import org.hamcrest.CoreMatchers
+import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic
 import org.springframework.test.annotation.IfProfileValue
 import org.springframework.test.annotation.ProfileValueSourceConfiguration
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -36,9 +43,15 @@ class UserControllerIntegrationTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+    @Autowired
+    private lateinit var staticResourceService: StaticResourceService
+
     private val controllerEndpoint = "/api/user"
 
     private val objectMapper = ObjectMapper()
+
+    @Value("classpath:testdata/testuserimage.png")
+    private lateinit var resource: Resource
 
     @Test
     fun getUser() {
@@ -59,5 +72,42 @@ class UserControllerIntegrationTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("$.username", CoreMatchers.`is`("registeruser")))
+    }
+
+    @Test
+    fun removeUser() {
+        mockMvc.perform(delete("$controllerEndpoint/admin")
+                .headers(TokenTestHelper.getTokenHeader(mockMvc)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk)
+    }
+
+    @Test
+    fun changePassword() {
+        val passwordDTO = PasswordDTO("newpass")
+        val json = objectMapper.writeValueAsString(passwordDTO)
+
+        mockMvc.perform(post("$controllerEndpoint")
+                .headers(TokenTestHelper.getTokenHeader(mockMvc))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(json))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk)
+    }
+
+    @Test
+    fun changeUserImage() {
+        val mockMultipartFile = MockMultipartFile("file", resource.file.readBytes())
+
+        val response = mockMvc.perform(multipart("$controllerEndpoint/image")
+                .file(mockMultipartFile)
+                .contentType(MediaType.IMAGE_PNG_VALUE)
+                .headers(TokenTestHelper.getTokenHeader(mockMvc)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk)
+                .andReturn()
+        val resource = JsonPath.parse(response.response.contentAsString).read<String>("$.image")
+
+        staticResourceService.removeStaticResource(resource)
     }
 }
